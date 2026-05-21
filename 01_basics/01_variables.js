ee75e6db3a23e12933517b877b04412b3c926c2f
ankit-codes-ece <ankitchaurasiya9451@gmail.com>
2026-05-21 02:18:21 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "ankitchau@google.com"
+var accountPassword = "12345"
+accountCity ="jaipur"
+let accountState
+
+//prefer not to use var
+// accountId = 2
+accountEmail ="afkd@google.com"
+accountPassword ="8386387"
+accountCity ="bengaluru"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all js code as newer version
+
+//alert(3 + 3) // we are using node JS not broser
+
+console.log(3 
+    +
+    3
+)  // dont use these type of shit, code readability should be high
+
+let name = "Ankit"
+let age = 18
+let isLoggedin = false
+
+
+// number 2^53
+//bigint 
+//string = ""
+//boolean = true, false
+//null = intentional absence of a value
+//undefined
+//symbol = unique
+
+
+//object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
