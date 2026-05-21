126a699c5a339ff4df7d522b2a1323cc6ea9c481
ankit-codes-ece <ankitchaurasiya9451@gmail.com>
2026-05-21 05:42:40 +0000

    conversion of operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,3 +18,32 @@ let boolIsLoggedIn = Boolean(IsLoggedIn)
 console.log(boolIsLoggedIn)
 
 // empty string -> false
+
+//****************operation****************\\
+
+
+let str1 = "Ankit "
+let str2 = "Chaurasiya"
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log(1 + 2);
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(2 ** 3);  // 2 ^ 3
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let abc = 100
+abc++
+console.log(abc);
+
+
+
+
