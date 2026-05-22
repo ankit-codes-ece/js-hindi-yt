384cc495726b16df0f765f7de3ec3828cddbc560
ankit-codes-ece <ankitchaurasiya9451@gmail.com>
2026-05-22 03:02:10 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0);  // in JS the equality check ( == ) and comparison check (>, <, >=, <=)
+// both work differently,,,,, comparison convert null to a number 0;
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// ===
+console.log("2" === 2);
